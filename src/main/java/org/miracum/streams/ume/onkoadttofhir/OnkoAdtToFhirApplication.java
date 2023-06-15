add01f2f1caa5c970aacbbdbb5bba32cb838f926
Marcel Erpenbeck <marcel.erpenbeck@uk-erlangen.de>
2023-06-15 16:05:43 +0200

    chore: bumps all dependencies

--- src/main/java/org/miracum/streams/ume/onkoadttofhir/OnkoAdtToFhirApplication.java
+++ src/main/java/org/miracum/streams/ume/onkoadttofhir/OnkoAdtToFhirApplication.java
@@ -1,21 +1,12 @@
 package org.miracum.streams.ume.onkoadttofhir;
 
-import org.apache.kafka.common.serialization.Serde;
-import org.hl7.fhir.instance.model.api.IBaseResource;
-import org.miracum.kafka.serializers.KafkaFhirSerde;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class OnkoAdtToFhirApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(OnkoAdtToFhirApplication.class, args);
-  }
-
-  @Bean
-  public Serde<IBaseResource> fhirSerde() {
-    return new KafkaFhirSerde();
   }
 }
