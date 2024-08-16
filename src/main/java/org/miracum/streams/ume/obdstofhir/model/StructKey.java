56d60307245209fa0801c5a96597181100c5f7c8
Paul-Christian Volkmer <code@pcvolkmer.de>
2024-08-16 10:29:44 +0200

    fix: add JsonProperty annotation with property names

--- src/main/java/org/miracum/streams/ume/obdstofhir/model/StructKey.java
+++ src/main/java/org/miracum/streams/ume/obdstofhir/model/StructKey.java
@@ -1,7 +1,11 @@
 package org.miracum.streams.ume.obdstofhir.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import java.io.Serializable;
 import lombok.Builder;
 
 @Builder
-public record StructKey(String referenzNummer, String tumorId) implements Serializable {}
+public record StructKey(
+    @JsonProperty("REFERENZ_NUMMER") String referenzNummer,
+    @JsonProperty("TUMOR_ID") String tumorId)
+    implements Serializable {}
