cabfc3cf516ce9f583febb9938d66250b8f9a9c3
Paul-Christian Volkmer <code@pcvolkmer.de>
2024-08-30 22:29:08 +0200

    feat: support capture groups in Pat-ID convert

--- src/test/java/org/miracum/streams/ume/obdstofhir/mapper/ObdsToFhirIntegrationTest.java
+++ src/test/java/org/miracum/streams/ume/obdstofhir/mapper/ObdsToFhirIntegrationTest.java
@@ -2,6 +2,7 @@ package org.miracum.streams.ume.obdstofhir.mapper;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -19,7 +20,30 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 @MockBean(FhirProperties.class)
 public class ObdsToFhirIntegrationTest {
 
-  @Autowired ObdsTestMapper mapper;
+  ObdsTestMapper mapper;
+
+  @BeforeEach
+  void setUp(@Autowired ObdsTestMapper mapper) {
+    this.mapper = mapper;
+  }
+
+  @Nested
+  @TestPropertySource(properties = {""})
+  class UseDefaultPatternWithoutConfig {
+
+    @ParameterizedTest
+    @CsvSource({
+      "12345,12345",
+      "123456789,123456789",
+      "1234567890,123456789", // Max 9 digits, remove last digit '0'
+      "1234567891,123456789", // Max 9 digits, remove last digit '1'
+      "0000012345,0000012345", // Not mathching pattern - keep as is
+    })
+    void applyDefaultPattern(String input, String output) {
+      var actual = ObdsToFhirMapper.convertId(input);
+      assertThat(actual).isEqualTo(output);
+    }
+  }
 
   @Nested
   @TestPropertySource(properties = {"app.patient-id-pattern=\\\\w*"})
@@ -40,17 +64,34 @@ public class ObdsToFhirIntegrationTest {
   }
 
   @Nested
-  class UseDefaultPatternWithoutConfig {
+  @TestPropertySource(properties = {"app.patient-id-pattern=G([0-9]{8})"})
+  class UsePatternWithCaptureGroups {
 
     @ParameterizedTest
     @CsvSource({
-      "12345,12345",
-      "123456789,123456789",
-      "1234567890,123456789", // Max 9 digits, remove last digit '0'
-      "1234567891,123456789", // Max 9 digits, remove last digit '1'
-      "0000012345,0000012345", // Not mathching pattern - keep as is
+      "12345,12345", // not matching, return as is ...
+      // else return first complete group of 8 numbers found in input after "G"
+      "G1234567890,12345678",
+      "G12345678,12345678",
     })
-    void applyDefaultPattern(String input, String output) {
+    void applyPatientIdPattern(String input, String output) {
+      var actual = ObdsToFhirMapper.convertId(input);
+      assertThat(actual).isEqualTo(output);
+    }
+  }
+
+  @Nested
+  @TestPropertySource(properties = {"app.patient-id-pattern=G/([0-9]{4})-([0-9]{4})"})
+  class UsePatternWithMultipleCaptureGroups {
+
+    @ParameterizedTest
+    @CsvSource({
+      "12345,12345", // not matching, return as is ...
+      // else return complete groups of 4 numbers found in input after "G/" and seperated by "-"
+      "G/1234-56789,12345678",
+      "G/1234-5678,12345678",
+    })
+    void applyPatientIdPattern(String input, String output) {
       var actual = ObdsToFhirMapper.convertId(input);
       assertThat(actual).isEqualTo(output);
     }
