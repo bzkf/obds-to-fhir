0ee4e8dc3d6f8cbe09090656260fae411581831e
Paul-Christian Volkmer <code@pcvolkmer.de>
2024-10-28 22:21:03 +0100

    refactor: use BeforeAll to set FhirProperties

--- src/test/java/org/miracum/streams/ume/obdstofhir/mapper/mii/PatientMapperTest.java
+++ src/test/java/org/miracum/streams/ume/obdstofhir/mapper/mii/PatientMapperTest.java
@@ -8,8 +8,10 @@ import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
 import com.fasterxml.jackson.module.jakarta.xmlbind.JakartaXmlBindAnnotationModule;
 import de.basisdatensatz.obds.v3.OBDS;
 import java.io.IOException;
+import java.util.TimeZone;
 import org.approvaltests.Approvals;
 import org.approvaltests.core.Options;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.miracum.streams.ume.obdstofhir.FhirProperties;
@@ -20,10 +22,11 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest(classes = {FhirProperties.class})
 @EnableConfigurationProperties
 class PatientMapperTest {
-  private final PatientMapper sut;
+  private static PatientMapper sut;
 
-  @Autowired
-  PatientMapperTest(FhirProperties fhirProps) {
+  @BeforeAll
+  static void beforeEach(@Autowired FhirProperties fhirProps) {
+    TimeZone.setDefault(TimeZone.getTimeZone("Europe/Berlin"));
     sut = new PatientMapper(fhirProps);
   }
 
