8dea896d59f0acaeb0903fd99fdd3d6bd236b263
Jasmin Ziegler <ziegler-jasmin@outlook.de>
2024-11-27 17:09:29 +0100

    now actually mapping and testing something

--- src/main/java/org/miracum/streams/ume/obdstofhir/mapper/mii/OperationMapper.java
+++ src/main/java/org/miracum/streams/ume/obdstofhir/mapper/mii/OperationMapper.java
@@ -30,9 +30,9 @@ public class OperationMapper extends ObdsToFhirMapper {
     var procedure = new Procedure();
 
     var identifier =
-    new Identifier()
-        .setSystem(fhirProperties.getSystems().getProcedureId())
-        .setValue(op.getOPID());
+        new Identifier()
+            .setSystem(fhirProperties.getSystems().getProcedureId())
+            .setValue(op.getOPID());
     procedure.addIdentifier(identifier);
     procedure.setId(computeResourceIdFromIdentifier(identifier));
 
@@ -41,49 +41,44 @@ public class OperationMapper extends ObdsToFhirMapper {
     // I would suggestion to always use COMPLETED here, any objections?
     procedure.setStatus(Procedure.ProcedureStatus.COMPLETED);
 
-    // there could be multiple OPS codes here --> create one coding for each, iterate through ops codes+
+    // there could be multiple OPS codes here --> create one coding for each, iterate through ops
+    // codes+
     // do I need distinct values = remove duplicates?
-    CodeableConcept opsCodeableConcept = new CodeableConcept()
+    CodeableConcept opsCodeableConcept = new CodeableConcept();
     for (var opsCode : op.getMengeOPS().getOPS()) {
 
-      Coding ops =
-        new Coding(fhirProperties.getSystems().getOps(), opsCode.getCode(), "");
+      Coding ops = new Coding(fhirProperties.getSystems().getOps(), opsCode.getCode(), "");
+      ops.setVersion(opsCode.getVersion());
       opsCodeableConcept.addCoding(ops);
-
-      }
-
-      // set all Codings gathered in the codeableconcept to code
-      procedure.setCode(opsCodeableConcept);
-
-
-      procedure.setSubject(subject);
-
-      // taken from SystTherapieProcedureMapper
-      var dataAbsentExtension =
-        new Extension(
-            fhirProperties.getExtensions().getDataAbsentReason(), new CodeType("unknown"));
-      var dataAbsentCode = new CodeType();
-      dataAbsentCode.addExtension(dataAbsentExtension);
-
-      var dateTime = convertObdsDatumToDateTimeType(op.getDatum());
-      if (dateTime.isPresent()) {
-        procedure.setPerformed(dateTime.get());
-    } else {
-        var performed = new DateTimeType();
-        performed.addExtension(dataAbsentExtension);
-        procedure.setPerformed(performed);
     }
 
+    // set all Codings gathered in the codeableconcept to code
+    procedure.setCode(opsCodeableConcept);
 
-      var intention = new CodeableConcept();
-      intention
-          .addCoding()
-          .setSystem(fhirProperties.getSystems().getMiiCsOnkoIntention())
-          .setCode(op.getIntention()); // Direct mapping from oBDS value
-      procedure.addExtension(
-          fhirProperties.getExtensions().getMiiExOnkoOPIntention(), intention);
+    procedure.setSubject(subject);
 
+    // taken from SystTherapieProcedureMapper
+    var dataAbsentExtension =
+        new Extension(
+            fhirProperties.getExtensions().getDataAbsentReason(), new CodeType("unknown"));
+    var dataAbsentCode = new CodeType();
+    dataAbsentCode.addExtension(dataAbsentExtension);
 
+    var dateTime = convertObdsDatumToDateTimeType(op.getDatum());
+    if (dateTime.isPresent()) {
+      procedure.setPerformed(dateTime.get());
+    } else {
+      var performed = new DateTimeType();
+      performed.addExtension(dataAbsentExtension);
+      procedure.setPerformed(performed);
+    }
+
+    var intention = new CodeableConcept();
+    intention
+        .addCoding()
+        .setSystem(fhirProperties.getSystems().getMiiCsOnkoIntention())
+        .setCode(op.getIntention()); // Direct mapping from oBDS value
+    procedure.addExtension(fhirProperties.getExtensions().getMiiExOnkoOPIntention(), intention);
 
     return procedure;
   }
--- src/test/java/org/miracum/streams/ume/obdstofhir/mapper/mii/OperationMapperTest.java
+++ src/test/java/org/miracum/streams/ume/obdstofhir/mapper/mii/OperationMapperTest.java
@@ -3,8 +3,7 @@ package org.miracum.streams.ume.obdstofhir.mapper.mii;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import ca.uhn.fhir.context.FhirContext;
-import com.fasterxml.jackson.core.exc.StreamReadException;
-import com.fasterxml.jackson.databind.DatabindException;
+import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.dataformat.xml.XmlMapper;
 import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
 import com.fasterxml.jackson.module.jakarta.xmlbind.JakartaXmlBindAnnotationModule;
@@ -12,7 +11,7 @@ import de.basisdatensatz.obds.v3.OBDS;
 import java.io.IOException;
 import java.util.TimeZone;
 import org.approvaltests.Approvals;
-import org.approvaltests.core.Options;
+import org.hl7.fhir.r4.model.Reference;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
@@ -33,27 +32,41 @@ public class OperationMapperTest {
   }
 
   @ParameterizedTest
-  @CsvSource({"obds3/test1.xml"})
-  void map_withGivenObds_shouldCreateValidProcedure(String sourceFile)
-      throws StreamReadException, DatabindException, IOException {
-    final var resource = this.getClass().getClassLoader().getResource(sourceFile);
+  @CsvSource({"Testpatient_1.xml", "Testpatient_2.xml"})
+  void map_withGivenObds_shouldCreateValidMedicationStatement(String sourceFile)
+      throws IOException {
+    final var resource = this.getClass().getClassLoader().getResource("obds3/" + sourceFile);
     assertThat(resource).isNotNull();
+
+    System.out.println("Loaded resource: " + resource.getPath());
 
     final var xmlMapper =
         XmlMapper.builder()
             .defaultUseWrapper(false)
             .addModule(new JakartaXmlBindAnnotationModule())
             .addModule(new Jdk8Module())
+            .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
             .build();
 
     final var obds = xmlMapper.readValue(resource.openStream(), OBDS.class);
 
     var obdsPatient = obds.getMengePatient().getPatient().getFirst();
 
-    // var procedure = sut.map(obdsPatient.getMengeMeldung().getMeldung().getFirst().getOP;
+    var subject = new Reference("Patient/any");
+    var condition = new Reference("Condition/any");
+
+    var opMeldung =
+        obdsPatient.getMengeMeldung().getMeldung().stream()
+            .filter(m -> m.getOP() != null)
+            .findFirst()
+            .get();
+
+    final var resultResource = sut.map(opMeldung.getOP(), subject, condition);
 
     var fhirParser = FhirContext.forR4().newJsonParser().setPrettyPrint(true);
-    var fhirJson = fhirParser.encodeResourceToString(procedure);
-    Approvals.verify(fhirJson, new Options().forFile().withExtension(".fhir.json"));
+    var fhirJson = fhirParser.encodeResourceToString(resultResource);
+    // System.out.println(fhirJson);
+    Approvals.verify(
+        fhirJson, Approvals.NAMES.withParameters(sourceFile).forFile().withExtension(".fhir.json"));
   }
 }
