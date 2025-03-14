134fda50c03a24dff4c47b7edbbaaa7749fe2f75
cfischer <clara1.fischer@ukr.de>
2025-03-14 08:53:14 +0100

    changed AbweichungPatientenwunsch

--- src/main/java/org/miracum/streams/ume/obdstofhir/mapper/mii/TumorkonferenzMapper.java
+++ src/main/java/org/miracum/streams/ume/obdstofhir/mapper/mii/TumorkonferenzMapper.java
@@ -1,8 +1,12 @@
 package org.miracum.streams.ume.obdstofhir.mapper.mii;
 
+import de.basisdatensatz.obds.v3.JNU;
+import de.basisdatensatz.obds.v3.JNUTyp;
 import de.basisdatensatz.obds.v3.TumorkonferenzTyp;
 import io.micrometer.common.lang.NonNull;
+
 import java.util.ArrayList;
+
 import org.hl7.fhir.r4.model.CarePlan;
 import org.hl7.fhir.r4.model.CodeableConcept;
 import org.hl7.fhir.r4.model.Coding;
@@ -21,16 +25,16 @@ public class TumorkonferenzMapper extends ObdsToFhirMapper {
   }
 
   public CarePlan map(
-      @NonNull TumorkonferenzTyp tk, @NonNull Reference patient, Reference primaerDiagnose) {
+    @NonNull TumorkonferenzTyp tk, @NonNull Reference patient, Reference primaerDiagnose) {
 
     CarePlan carePlan = new CarePlan();
     // Meta
     carePlan.getMeta().addProfile(fhirProperties.getProfiles().getMiiPrOnkoTumorkonferenz());
     // Identifier + Id
     final Identifier identifier =
-        new Identifier()
-            .setSystem(fhirProperties.getSystems().getTumorkonferenzId())
-            .setValue("Tumorkonferenz_" + tk.getTumorkonferenzID());
+      new Identifier()
+        .setSystem(fhirProperties.getSystems().getTumorkonferenzId())
+        .setValue("Tumorkonferenz_" + tk.getTumorkonferenzID());
     carePlan.addIdentifier(identifier);
     carePlan.setId(computeResourceIdFromIdentifier(identifier));
     // Status
@@ -43,10 +47,10 @@ public class TumorkonferenzMapper extends ObdsToFhirMapper {
     carePlan.setIntent(CarePlan.CarePlanIntent.PLAN);
     // category
     CodeableConcept codeableConcept =
-        new CodeableConcept(
-            new Coding()
-                .setSystem(fhirProperties.getSystems().getMiiCsOnkoTherapieplanungTyp())
-                .setCode(tk.getTyp()));
+      new CodeableConcept(
+        new Coding()
+          .setSystem(fhirProperties.getSystems().getMiiCsOnkoTherapieplanungTyp())
+          .setCode(tk.getTyp()));
     carePlan.addCategory(codeableConcept);
     // subject
     carePlan.setSubject(patient);
@@ -61,26 +65,30 @@ public class TumorkonferenzMapper extends ObdsToFhirMapper {
     //  detail.code
     if (tk.getTherapieempfehlung() != null) {
       for (String typ :
-          tk.getTherapieempfehlung().getMengeTypTherapieempfehlung().getTypTherapieempfehlung()) {
-
-        // CarePlan.CarePlanActivityComponent activityComponent = new
-        // CarePlan.CarePlanActivityComponent();
+        tk.getTherapieempfehlung().getMengeTypTherapieempfehlung().getTypTherapieempfehlung()) {
         CodeableConcept therapieEmpfehlungen =
-            new CodeableConcept(
-                new Coding()
-                    .setSystem(fhirProperties.getSystems().getMiiCsOnkoTherapieTyp())
-                    .setCode(typ));
-        CarePlan.CarePlanActivityDetailComponent cpadc =
-            new CarePlan.CarePlanActivityDetailComponent();
+          new CodeableConcept(
+            new Coding()
+              .setSystem(fhirProperties.getSystems().getMiiCsOnkoTherapieTyp())
+              .setCode(typ));
+
+        CarePlan.CarePlanActivityDetailComponent cpadc = new CarePlan.CarePlanActivityDetailComponent();
         cpadc.setCode(therapieEmpfehlungen);
         // detail.Status
-        cpadc.setStatus(CarePlan.CarePlanActivityStatus.COMPLETED);
+        CodeableConcept statusReason = new CodeableConcept(new Coding().setSystem(fhirProperties.getSystems().getMiiCsOnkoTherapieabweichung()).setCode(tk.getTherapieempfehlung().getAbweichungPatientenwunsch().value()));
+        switch (tk.getTherapieempfehlung().getAbweichungPatientenwunsch()) {
+          case JNU.J -> cpadc.setStatus(CarePlan.CarePlanActivityStatus.CANCELLED);
+          case JNU.N -> {
+            if (carePlan.getStatus() == CarePlan.CarePlanStatus.COMPLETED) {
+              cpadc.setStatus(CarePlan.CarePlanActivityStatus.COMPLETED);
+            } else if (carePlan.getStatus() == CarePlan.CarePlanStatus.ACTIVE) {
+              cpadc.setStatus(CarePlan.CarePlanActivityStatus.INPROGRESS);
+            }
+          }
+          case JNU.U -> cpadc.setStatus(CarePlan.CarePlanActivityStatus.UNKNOWN);
+        }
         // detail.StatusReason
-        cpadc.setStatusReason(
-            new CodeableConcept(
-                new Coding()
-                    .setSystem(fhirProperties.getSystems().getMiiCsOnkoTherapieabweichung())
-                    .setCode(tk.getTherapieempfehlung().getAbweichungPatientenwunsch().value())));
+        cpadc.setStatusReason(statusReason);
         // add Activity.Detail
         carePlan.addActivity().setDetail(cpadc);
       }
