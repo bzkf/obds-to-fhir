7a93eb4f18ebc74adc79aca0a3686d7a56e65ae0
Paul-Christian Volkmer <code@pcvolkmer.de>
2025-03-26 11:08:16 +0100

    build: add obds2-to-obds3
    This includes Spring bean creation using ObdsMapper builder that ignores
    unmappable items.

--- src/main/java/org/miracum/streams/ume/obdstofhir/ObdsToFhirApplication.java
+++ src/main/java/org/miracum/streams/ume/obdstofhir/ObdsToFhirApplication.java
@@ -1,12 +1,19 @@
 package org.miracum.streams.ume.obdstofhir;
 
+import dev.pcvolkmer.onko.obds2to3.ObdsMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @ConfigurationPropertiesScan
 public class ObdsToFhirApplication {
+
+  @Bean
+  public ObdsMapper obdsMapper() {
+    return ObdsMapper.builder().ignoreUnmappable(true).build();
+  }
 
   public static void main(String[] args) {
     SpringApplication.run(ObdsToFhirApplication.class, args);
