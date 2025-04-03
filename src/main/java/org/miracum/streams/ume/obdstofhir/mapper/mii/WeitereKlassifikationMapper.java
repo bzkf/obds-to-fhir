65f9dc4b7086d31b0bead77bfe8d046049b30d79
chgl <chgl@users.noreply.github.com>
2025-04-03 17:51:23 +0200

    feat: added weitere klassifikationen mapper

--- src/main/java/org/miracum/streams/ume/obdstofhir/mapper/mii/WeitereKlassifikationMapper.java
+++ src/main/java/org/miracum/streams/ume/obdstofhir/mapper/mii/WeitereKlassifikationMapper.java
@@ -1,11 +1,9 @@
 package org.miracum.streams.ume.obdstofhir.mapper.mii;
 
-import com.github.slugify.Slugify;
 import de.basisdatensatz.obds.v3.MengeWeitereKlassifikationTyp;
 import java.util.ArrayList;
 import java.util.List;
 import org.hl7.fhir.r4.model.CodeableConcept;
-import org.hl7.fhir.r4.model.Coding;
 import org.hl7.fhir.r4.model.Enumerations.ResourceType;
 import org.hl7.fhir.r4.model.Identifier;
 import org.hl7.fhir.r4.model.Observation;
@@ -20,13 +18,9 @@ import org.springframework.stereotype.Service;
 public class WeitereKlassifikationMapper extends ObdsToFhirMapper {
 
   private static final Logger LOG = LoggerFactory.getLogger(WeitereKlassifikationMapper.class);
-  private static final Slugify slugify = Slugify.builder().build();
-  private final WeitereKlassifikationenMappings mappings;
 
-  public WeitereKlassifikationMapper(
-      FhirProperties fhirProperties, WeitereKlassifikationenMappings mappings) {
+  public WeitereKlassifikationMapper(FhirProperties fhirProperties) {
     super(fhirProperties);
-    this.mappings = mappings;
   }
 
   public List<Observation> map(
@@ -47,7 +41,7 @@ public class WeitereKlassifikationMapper extends ObdsToFhirMapper {
           .addProfile(fhirProperties.getProfiles().getMiiPrOnkoWeitereKlassifikationen());
 
       // Identifiers
-      var identifierValue = slugify.slugify(meldungId + "-" + klassifikation.getName());
+      var identifierValue = meldungId + "-" + slugifier.slugify(klassifikation.getName());
       var identifier =
           new Identifier()
               .setSystem(fhirProperties.getSystems().getWeitereKlassifikationObservationId())
@@ -68,31 +62,12 @@ public class WeitereKlassifikationMapper extends ObdsToFhirMapper {
       convertObdsDatumToDateTimeType(klassifikation.getDatum())
           .ifPresent(observation::setEffective);
 
-      if (mappings.getWeitereKlassifikationen().containsKey(klassifikation.getName())) {
-        var mapping = mappings.getWeitereKlassifikationen().get(klassifikation.getName());
-        observation.setCode(new CodeableConcept(mapping.coding()));
+      var code = new CodeableConcept().setText(klassifikation.getName());
+      observation.setCode(code);
 
-        var value = mapping.mappings().get(klassifikation.getStadium());
-
-        if (value != null) {
-          observation.setValue(new CodeableConcept(value));
-        } else {
-          LOG.warn(
-              "Stadium {} doesn't exist in mapping config for {}. Setting value as plain code.",
-              klassifikation.getStadium(),
-              klassifikation.getName());
-          value = new Coding().setCode(klassifikation.getStadium());
-          observation.setValue(new CodeableConcept(value));
-        }
-
-      } else {
-        var code = new CodeableConcept().setText(klassifikation.getName());
-        observation.setCode(code);
-
-        var value = new CodeableConcept();
-        value.addCoding().setCode(klassifikation.getStadium());
-        observation.setValue(value);
-      }
+      var value = new CodeableConcept();
+      value.addCoding().setCode(klassifikation.getStadium());
+      observation.setValue(value);
 
       observations.add(observation);
     }
