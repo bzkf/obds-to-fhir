74988b0988db2418976527074138b3ae8c1bb00d
chgl <chgl@users.noreply.github.com>
2025-05-31 00:34:55 +0200

    ci: added dq checks based on gx (#340)
    * wip
    
    * move bundle tests to dedicated folder
    
    * feat: added dq checks
    
    * rm pwd
    
    * nl
    
    * .txt
    
    * lint
    
    * version
    
    * wip
    
    * feat: more dq checks added
    
    * min_max_date
    
    * refactor
    
    * refactor
    
    * dq
    
    * validate bundles seperately
    
    * depth 1

---
+++ src/main/python/check_dq.py
@@ -0,0 +1,338 @@
+import os
+import sys
+from datetime import datetime
+from pathlib import Path
+
+import great_expectations as gx
+import pandas as pd
+from great_expectations.checkpoint import UpdateDataDocsAction
+from great_expectations.core.result_format import ResultFormat
+from loguru import logger
+from pathling import Expression as exp
+from pathling import PathlingContext
+from pyspark.sql.functions import col, concat, lit
+
+pc = PathlingContext.create(enable_extensions=True, enable_delta=True)
+
+HERE = Path(os.path.abspath(os.path.dirname(__file__)))
+ICD_10_GM_SYSTEM = "http://fhir.de/CodeSystem/bfarm/icd-10-gm"
+ASSERTED_DATE_EXTENSION = (
+    "http://hl7.org/fhir/StructureDefinition/condition-assertedDate"
+)
+MII_PR_ONKO_WEITERE_KLASSIFIKATIONEN = (
+    "https://www.medizininformatik-initiative.de/"
+    + "fhir/ext/modul-onko/StructureDefinition/"
+    + "mii-pr-onko-weitere-klassifikationen"
+)
+MIN_DATE = pd.Timestamp("1900-01-01")
+MAX_DATE = pd.Timestamp(year=datetime.now().year, month=12, day=31)
+
+snapshots_dir = (
+    HERE
+    / "../../test/java/snapshots/org/miracum/streams/ume/obdstofhir/"
+    / "mapper/mii/ObdsToFhirBundleMapperTest/"
+).as_posix()
+
+data = pc.read.bundles(
+    snapshots_dir, ["Patient", "Condition", "Observation", "Procedure"]
+)
+
+patients = data.extract(
+    "Patient",
+    columns=[
+        exp("id", "patient_id"),
+        exp("gender", "gender"),
+        exp("birthDate", "date_of_birth"),
+        exp("deceasedDateTime", "deceased_date_time"),
+    ],
+).drop_duplicates()
+
+patients.show(truncate=False)
+
+
+conditions = data.extract(
+    "Condition",
+    columns=[
+        exp("id", "condition_id"),
+        exp(
+            f"code.coding.where(system='{ICD_10_GM_SYSTEM}').code",
+            "icd_code",
+        ),
+        exp(
+            f"code.coding.where(system='{ICD_10_GM_SYSTEM}').version",
+            "icd_version",
+        ),
+        exp("subject.reference", "subject_reference"),
+        exp(
+            f"extension('{ASSERTED_DATE_EXTENSION}').valueDateTime",
+            "asserted_date",
+        ),
+        exp("recordedDate", "recorded_date"),
+    ],
+).drop_duplicates()
+
+# (full) outer join to retain null values if either side is missing
+patients_with_conditions = conditions.join(
+    patients,
+    conditions["subject_reference"] == concat(lit("Patient/"), col("patient_id")),
+    how="outer",
+)
+
+patients_with_conditions.show(truncate=False)
+
+observations = data.extract(
+    "Observation",
+    columns=[
+        exp("id", "observation_id"),
+        exp("subject.reference", "subject_reference"),
+        exp("code.coding.system", "code_system"),
+        exp("code.coding.code", "code_code"),
+        exp(
+            """
+                Observation.where(
+                    code.coding.exists(
+                        system='http://loinc.org' or
+                        system='http://snomed.info/sct')
+                ).valueCodeableConcept.coding.code
+            """,
+            "value_codeable_concept_coding_code",
+        ),
+        exp("effectiveDateTime", "effective_date_time"),
+        exp("meta.profile", "meta_profile"),
+    ],
+).drop_duplicates()
+
+patients_with_observations = observations.join(
+    patients,
+    observations["subject_reference"] == concat(lit("Patient/"), col("patient_id")),
+    how="outer",
+)
+
+patients_with_observations.show(truncate=False)
+
+patients_with_observations.write.mode("overwrite").csv(
+    "patients_with_observations.csv", header=True
+)
+
+gx_context = gx.get_context(mode="file")
+
+
+def create_expectations_and_suite(expectations, suite_name, gx_context):
+    suite = gx.ExpectationSuite(name=suite_name)
+    for expectation in expectations:
+        suite.add_expectation(expectation)
+    suite = gx_context.suites.add_or_update(suite=suite)
+    return suite
+
+
+def create_validation_definition(gx_context, data_asset, suite, name, batch_name):
+    return gx_context.validation_definitions.add_or_update(
+        gx.ValidationDefinition(
+            name=name,
+            data=data_asset.add_batch_definition_whole_dataframe(batch_name),
+            suite=suite,
+        )
+    )
+
+
+def create_checkpoint(gx_context, name, validation_definitions, action_list):
+    return gx_context.checkpoints.add_or_update(
+        gx.Checkpoint(
+            name=name,
+            validation_definitions=validation_definitions,
+            actions=action_list,
+            result_format=ResultFormat.COMPLETE,
+        )
+    )
+
+
+# Define expectations for each dataset
+expectations_conditions = [
+    # Birth date validations
+    gx.expectations.ExpectColumnValuesToBeBetween(
+        column="date_of_birth", min_value=MIN_DATE, max_value=MAX_DATE
+    ),
+    # Deceased date validations
+    gx.expectations.ExpectColumnPairValuesAToBeGreaterThanB(
+        column_A="deceased_date_time",
+        column_B="date_of_birth",
+        ignore_row_if="either_value_is_missing",
+    ),
+    gx.expectations.ExpectColumnValuesToBeBetween(
+        column="deceased_date_time", min_value=MIN_DATE, max_value=MAX_DATE
+    ),
+    # asserted datetime validations
+    gx.expectations.ExpectColumnValuesToBeBetween(
+        column="asserted_date", min_value=MIN_DATE, max_value=MAX_DATE
+    ),
+    gx.expectations.ExpectColumnPairValuesAToBeGreaterThanB(
+        column_A="asserted_date",
+        column_B="date_of_birth",
+        or_equal=True,
+        description="Check if diagnosis asserted date is on or after the date of birth",
+    ),
+    # Gender expectation with condition code
+    gx.expectations.ExpectColumnValuesToBeInSet(
+        column="gender",
+        value_set=["male"],
+        condition_parser="great_expectations",
+        row_condition='col("icd_code") == "C61"',
+    ),
+    gx.expectations.ExpectColumnValuesToNotBeNull(
+        column="patient_id",
+    ),
+    gx.expectations.ExpectColumnValuesToNotBeNull(
+        column="condition_id",
+    ),
+    gx.expectations.ExpectColumnValuesToNotBeNull(
+        column="icd_version", description="Check if ICD version is always provided"
+    ),
+    gx.expectations.ExpectColumnValuesToNotBeNull(
+        column="icd_code",
+        description="Check if ICD code is always provided.",
+    ),
+    gx.expectations.ExpectColumnPairValuesAToBeGreaterThanB(
+        column_A="recorded_date",
+        column_B="date_of_birth",
+        or_equal=True,
+        description="Check if diagnosis recorded date is on or after the date of birth",
+    ),
+]
+
+expectations_observations = [
+    # Birth date validations
+    gx.expectations.ExpectColumnValuesToBeBetween(
+        column="date_of_birth", min_value=MIN_DATE, max_value=MAX_DATE
+    ),
+    gx.expectations.ExpectColumnValuesToNotBeNull(
+        column="code_system",
+        condition_parser="great_expectations",
+        row_condition='col("meta_profile") != '
+        + f'"{MII_PR_ONKO_WEITERE_KLASSIFIKATIONEN}"',
+    ),
+    gx.expectations.ExpectColumnValuesToNotBeNull(
+        column="code_code",
+        condition_parser="great_expectations",
+        row_condition='col("meta_profile") != '
+        + f'"{MII_PR_ONKO_WEITERE_KLASSIFIKATIONEN}"',
+    ),
+    gx.expectations.ExpectColumnValuesToNotBeNull(
+        column="patient_id",
+    ),
+    gx.expectations.ExpectColumnValuesToNotBeNull(
+        column="observation_id",
+    ),
+    # EffectiveDateTime validations
+    gx.expectations.ExpectColumnPairValuesAToBeGreaterThanB(
+        column_A="effective_date_time",
+        column_B="date_of_birth",
+        or_equal=True,
+    ),
+    gx.expectations.ExpectColumnPairValuesAToBeGreaterThanB(
+        column_A="deceased_date_time",
+        column_B="effective_date_time",
+        or_equal=True,
+    ),
+    gx.expectations.ExpectColumnValuesToBeBetween(
+        column="effective_date_time", min_value=MIN_DATE, max_value=MAX_DATE
+    ),
+]
+
+# Create expectation suites
+suite_conditions = create_expectations_and_suite(
+    expectations_conditions, "patients_with_conditions_suite", gx_context
+)
+suite_observations = create_expectations_and_suite(
+    expectations_observations, "patients_with_observations_suite", gx_context
+)
+
+data_source_name = "snapshot_bundles"
+data_source = gx_context.data_sources.add_or_update_spark(name=data_source_name)
+data_asset = data_source.add_dataframe_asset(name="patients_and_conditions")
+data_asset_observations = data_source.add_dataframe_asset(
+    name="patients_and_observations"
+)
+
+# Add the Batch Definition and Validation Definitions
+validation_definition_conditions = create_validation_definition(
+    gx_context,
+    data_asset,
+    suite_conditions,
+    "validate_conditions",
+    "patients_and_conditions_all_rows",
+)
+validation_definition_observations = create_validation_definition(
+    gx_context,
+    data_asset_observations,
+    suite_observations,
+    "validate_observations",
+    "patients_and_observations_all_rows",
+)
+
+# Actions for checkpoints
+action_list = [
+    UpdateDataDocsAction(
+        name="update_all_data_docs",
+    ),
+]
+
+# Create checkpoints
+checkpoint_conditions = create_checkpoint(
+    gx_context,
+    "validate_conditions_checkpoint",
+    [validation_definition_conditions],
+    action_list,
+)
+checkpoint_observations = create_checkpoint(
+    gx_context,
+    "validate_observations_checkpoint",
+    [validation_definition_observations],
+    action_list,
+)
+
+# Add Data Docs Site
+site_name = "obds_to_fhir_data_docs_site"
+base_directory = "uncommitted/data_docs/local_site/"
+site_config = {
+    "class_name": "SiteBuilder",
+    "store_backend": {
+        "class_name": "TupleFilesystemStoreBackend",
+        "base_directory": base_directory,
+    },
+    "site_index_builder": {"class_name": "DefaultSiteIndexBuilder"},
+}
+
+# this only has to be done once, after that the site is persisted to the file system.
+# and otherwise you also get:
+# InvalidKeyError: Data Docs Site `obds_to_fhir_data_docs_site` already exists
+# in the Data Context.
+# gx_context.add_data_docs_site(site_name=site_name, site_config=site_config)
+
+gx_context.update_data_docs_site(
+    site_name=site_name,
+    site_config=site_config,
+)
+
+# Build the Data Docs
+gx_context.build_data_docs()
+
+# Run the Checkpoints
+validation_results_conditions = checkpoint_conditions.run(
+    batch_parameters={"dataframe": patients_with_conditions}
+)
+
+logger.info(validation_results_conditions.describe())
+
+if not validation_results_conditions.success:
+    logger.error("Validation run failed!")
+    sys.exit(1)
+
+validation_results_observations = checkpoint_observations.run(
+    batch_parameters={"dataframe": patients_with_observations}
+)
+
+logger.info(validation_results_observations.describe())
+
+if not validation_results_observations.success:
+    logger.error("Validation run failed!")
+    sys.exit(1)
--- src/test/java/org/miracum/streams/ume/obdstofhir/mapper/mii/ObdsToFhirBundleMapperTest.java
+++ src/test/java/org/miracum/streams/ume/obdstofhir/mapper/mii/ObdsToFhirBundleMapperTest.java
@@ -2,8 +2,11 @@ package org.miracum.streams.ume.obdstofhir.mapper.mii;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import ca.uhn.fhir.context.FhirContext;
+import com.spun.util.introspection.Caller;
 import de.basisdatensatz.obds.v3.OBDS;
 import java.io.IOException;
+import org.approvaltests.Approvals;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
@@ -75,6 +78,22 @@ class ObdsToFhirBundleMapperTest extends MapperTest {
 
     final var bundles = sut.map(obds);
 
-    verifyAll(bundles, sourceFile);
+    var caller = Caller.get(0);
+    var methodName = caller.getMethodName();
+    var className = this.getClass().getSimpleName();
+
+    var fhirParser = FhirContext.forR4().newJsonParser().setPrettyPrint(true);
+
+    for (int i = 0; i < bundles.size(); i++) {
+      var fhirJson = fhirParser.encodeResourceToString(bundles.get(i));
+      Approvals.verify(
+          fhirJson,
+          Approvals.NAMES
+              .withParameters("")
+              .forFile()
+              .withBaseName(String.format("%s/%s.%s.%d", className, methodName, sourceFile, i))
+              .forFile()
+              .withExtension(".fhir.json"));
+    }
   }
 }
