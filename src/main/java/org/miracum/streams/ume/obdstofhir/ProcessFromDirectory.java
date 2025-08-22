a05e959df0a903a19291bb82234ccc9ef929424c
Marcel Erpenbeck <marcel.erpenbeck@uk-erlangen.de>
2025-08-22 13:46:49 +0200

    feat: split collection of meldungen in single medlungen before processing in ProcessFromDirectory

--- src/main/java/org/miracum/streams/ume/obdstofhir/ProcessFromDirectory.java
+++ src/main/java/org/miracum/streams/ume/obdstofhir/ProcessFromDirectory.java
@@ -1,11 +1,15 @@
 package org.miracum.streams.ume.obdstofhir;
 
 import ca.uhn.fhir.context.FhirContext;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import de.basisdatensatz.obds.v3.OBDS;
+import dev.pcvolkmer.onko.obds2to3.ObdsMapper;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
@@ -13,9 +17,9 @@ import java.util.concurrent.TimeoutException;
 import java.util.stream.Collectors;
 import org.hl7.fhir.instance.model.api.IBaseResource;
 import org.hl7.fhir.r4.model.Bundle;
-import org.miracum.streams.ume.obdstofhir.mapper.mii.ObdsToFhirBundleMapper;
 import org.miracum.streams.ume.obdstofhir.model.MeldungExportListV3;
 import org.miracum.streams.ume.obdstofhir.model.MeldungExportV3;
+import org.miracum.streams.ume.obdstofhir.model.ObdsOrAdt;
 import org.miracum.streams.ume.obdstofhir.processor.MeldungTransformationService;
 import org.miracum.streams.ume.obdstofhir.serde.Obdsv3Deserializer;
 import org.slf4j.Logger;
@@ -36,21 +40,23 @@ public class ProcessFromDirectory {
   private static final Logger LOG = LoggerFactory.getLogger(ProcessFromDirectory.class);
   private static final FhirContext fhirContext = FhirContext.forR4();
 
+  private final ObjectMapper objectMapper = new ObjectMapper();
+
   private ProcessFromDirectoryConfig config;
   private KafkaTemplate<String, IBaseResource> kafkaTemplate;
-  private ObdsToFhirBundleMapper mapper;
   private Obdsv3Deserializer deserializer;
   private MeldungTransformationService meldungTransformationService;
+  private ObdsMapper obdsV2ToV3Mapper;
 
   public ProcessFromDirectory(
       ProcessFromDirectoryConfig config,
       KafkaTemplate<String, IBaseResource> kafkaTemplate,
-      ObdsToFhirBundleMapper mapper,
+      ObdsMapper obdsMapper,
       Obdsv3Deserializer deserializer,
       MeldungTransformationService meldungTransformationService) {
     this.config = config;
     this.kafkaTemplate = kafkaTemplate;
-    this.mapper = mapper;
+    this.obdsV2ToV3Mapper = obdsMapper;
     this.meldungTransformationService = meldungTransformationService;
     this.deserializer = deserializer;
   }
@@ -75,15 +81,20 @@ public class ProcessFromDirectory {
         var obdsOrAdt = deserializer.deserializeAsObdsOrAdt(xmlString);
 
         MeldungExportV3 meldung = new MeldungExportV3();
-        if (obdsOrAdt.hasADT() || obdsOrAdt.hasOBDS()) {
+        if (obdsOrAdt.hasOBDS()) {
           meldung.setObdsOrAdt(obdsOrAdt);
-          meldung.setVersionsnummer(1);
+        } else if (obdsOrAdt.hasADT()) {
+          var adt = obdsOrAdt.getAdt();
+          var obds = obdsV2ToV3Mapper.map(adt);
+          meldung.setObdsOrAdt(ObdsOrAdt.from(obds));
         } else {
           LOG.warn("No OBDS or ADT_GEKID found in file. Ignoring and continuing.");
           continue;
         }
 
-        meldungExportV3List.addElement(meldung);
+        for (var einzelMeldung : splitSammelmeldungen(meldung)) {
+          meldungExportV3List.addElement(einzelMeldung);
+        }
       }
 
       List<Bundle> bundles = meldungTransformationService.processDirMeldungen(meldungExportV3List);
@@ -122,5 +133,55 @@ public class ProcessFromDirectory {
       MDC.clear();
     }
     readyEvent.getApplicationContext().close();
+  }
+
+  private List<MeldungExportV3> splitSammelmeldungen(MeldungExportV3 sammelMeldung) {
+    List<MeldungExportV3> einzelMeldungResult = new ArrayList<>();
+
+    if (sammelMeldung.getObdsOrAdt().hasOBDS() && sammelMeldung.getObdsOrAdt().hasADT()) {
+      return einzelMeldungResult; // no obds or adt
+    }
+
+    var obds = sammelMeldung.getObds();
+    var adt = sammelMeldung.getObdsOrAdt().getAdt();
+    if ((adt == null || adt.getMengePatient() == null)
+        && (obds == null || obds.getMengePatient() == null)) {
+      return einzelMeldungResult; // no patients
+    }
+
+    for (var patient : sammelMeldung.getObdsOrAdt().getObds().getMengePatient().getPatient()) {
+      for (var meldung : patient.getMengeMeldung().getMeldung()) {
+
+        // copy obds
+        var obdsCopy = objectMapper.convertValue(obds, OBDS.class);
+
+        // find corresponding patient in copy
+        var patientCopy =
+            obdsCopy.getMengePatient().getPatient().stream()
+                .filter(p -> p.getPatientID().equals(patient.getPatientID()))
+                .findFirst()
+                .orElseThrow();
+
+        // replace MengeMeldung with single meldung
+        patientCopy.getMengeMeldung().getMeldung().clear();
+        patientCopy.getMengeMeldung().getMeldung().add(meldung);
+
+        // replace patient
+        obdsCopy.getMengePatient().getPatient().clear();
+        obdsCopy.getMengePatient().getPatient().add(patientCopy);
+
+        var newMeldung =
+            MeldungExportV3.builder()
+                .id(String.valueOf(Math.random()))
+                .referenzNummer(patient.getPatientID())
+                .lkrMeldung(meldung.getMeldungID())
+                .versionsnummer(1)
+                .obdsOrAdt(ObdsOrAdt.from(obdsCopy))
+                .build();
+
+        einzelMeldungResult.add(newMeldung);
+      }
+    }
+    return einzelMeldungResult;
   }
 }
